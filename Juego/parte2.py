07ea2666a548b2bbed34047861001d0d82c8bfbd
Juan Aycardi <Juan David Aycardi Ariza>
2022-09-04 17:23:45 -0500

    State Machine

---
+++ Juego/parte2.py
@@ -0,0 +1,58 @@
+
+
+def stateMachine2():
+	state = 19
+	end = True
+	while end:
+
+		if state == 19:
+
+
+		elif state == 20:
+
+
+		elif state == 21:
+
+
+		elif state == 22:
+
+
+		elif state == 23:
+
+
+		elif state == 24:
+
+
+		elif state == 25:
+
+
+		elif state == 26:
+
+
+		elif state == 27:
+
+
+		elif state == 28:
+
+
+		elif state == 29:
+
+
+		elif state == 30:
+
+
+		elif state == 31:
+
+
+		elif state == 32:
+
+
+		elif state == 33:
+
+
+		elif state == 34:
+
+
+		elif state == 35:
+
+
