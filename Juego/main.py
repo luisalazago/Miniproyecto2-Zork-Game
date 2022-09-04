4d51a3f3689e494c168243a95a03af34723caf72
Juan Aycardi <Juan David Aycardi Ariza>
2022-09-04 16:58:44 -0500

    Delay Print

--- Juego/main.py
+++ Juego/main.py
@@ -3,25 +3,35 @@ Nombre: El Misterio de Albus.
 Por: Luis Alberto Salazar y Juan David Aycardi.
 """
 
+import time
+import sys
+
+def dprint(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.15)
+    print()
+
 def main():
     flag = True
     while(flag):
         print("==================================================================")
-        print("EL MISTERIO")
-        print("DE")
-        print("ALBUS")
+        dprint("EL MISTERIO")
+        dprint("DE")
+        dprint("ALBUS")
         print("==================================================================")
-        print("Menu de opciones")
+        print("Menú de opciones")
         print("__________________________________________________________________")
         print("1) Iniciar nueva partida")
         print("2) Continuar partida")
         print("3) Salir del juego")
         print("__________________________________________________________________")
-        x = int(input("¿Que opcion desea seleccionar?: "))
+        x = int(input("¿Qué opción desea seleccionar?: "))
         while(x < 1 or x > 3):
             print("")
-            print("Error, por favor seleccion una opcion valida")
-            x = int(input("¿Que opcion desea seleccionar?: "))
+            print("Error, por favor seleccione una opción válida")
+            x = int(input("¿Qué opción desea seleccionar?: "))
         
         print("")
         if(x == 1): print("Iniciando una nueva partida porque aun no hay funciones")
