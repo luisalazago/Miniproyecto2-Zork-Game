807094c5864d646fabfadc9d34d649e262e05bcf
Luis <luisalazago@gmail.com>
2022-09-19 23:21:40 -0500

    Arreglo de funciones de carga y otras de juego

--- Juego/parte1.py
+++ Juego/parte1.py
@@ -6,12 +6,22 @@ from distutils.errors import LibError
 from os import system
 import time
 
-comandos_niveles = [["go", "stay"], ["follow", "return"]]
-formato_comandos = {"go": ["name"], "stay": [], "follow": ["name"], "return": ["name"]}
+# Variables Globales
+comandos_niveles = [
+    ["go", "stay"],  ["follow", "return"], 
+    ["take", "straight"], ["send", "go"]
+]
+formato_comandos = {
+    "go": [["alderaan", "home"], 1], "stay": [[], 0], 
+    "follow": [["guy"], 1], "return": [["home"], 1],
+    "send": [["message"], 1], "take": [["atajo"], 1],
+    "straight": [[], 0]
+}
 contador = 0
 contador_derecha = 0
 contador_izquierda = 0
 
+# Funciones para el final
 def imprimirFinal():
     system("cls")
     print("======================================================================================================")
@@ -21,13 +31,16 @@ def imprimirFinal():
     print("======================================================================================================")
     time.sleep(7)
 
-def evaluar_comando(comando, texto):
+def evaluar_comando(comando):
     # Primero se evalua si se envio el formato adecuado del comando por parte del usuario.
-    if(len(formato_comandos[comando[0]]) + 1 != len(comando)): return True
-    veracidad = False
-    for condiciones in formato_comandos[comando[0]]:
-        if(condiciones == "name" and comando[1] != texto): veracidad = True
-    return veracidad
+    ans = None
+    if(not formato_comandos[comando[0]][1]): ans = False
+    elif(formato_comandos[comando[0]][1] + 1 != len(comando)):
+        ans = True
+    else:
+        ans = False
+        if(not comando[1] in formato_comandos[comando[0]][0]): ans = True
+    return ans
 
 # Funciones para cargar el texto de los archivos
 def cargarTexto():
@@ -51,70 +64,82 @@ def cargarTextoDerecha():
     archivo = open("../Juego/Historia_to_load/parte1/decision_derecha.txt", "r", encoding = 'utf-8')
     archivo = archivo.readlines()
     archivo = archivo[contador_derecha:]
+    print("")
     for linea in archivo:
         contador_derecha += 1
         if(linea[0] == "+"): break
         print(linea)
-    contador_derecha += 1
-    
+
 def cargarTextoIzquierda():
     global contador_izquierda
     archivo = open("../Juego/Historia_to_load/parte1/decision_izquierda.txt", "r", encoding = 'utf-8')
     archivo = archivo.readlines()
     archivo = archivo[contador_izquierda:]
+    print("")
     for linea in archivo:
         contador_izquierda += 1
         if(linea[0] == "-"): break
         print(linea)
-    contador_izquierda += 1
+
+# Funciones para jugar
+def juego(state):
+    cargarTexto()
+    x = input("/> ")
+    x = x.lower().split()
+    veracidad = True
+    while(veracidad):
+        if(x[0] in comandos_niveles[state - 1]):
+            for comando in comandos_niveles[state - 1]:
+                if(x[0] == comando): veracidad = evaluar_comando(x)
+        if(not veracidad): break
+        print("Comando no reconocido.")
+        x = input("/> ")
+        x = x.lower().split()
+    return x
 
 # Función principal
 def stateMachine():
+    global contador, contador_izquierda, contador_derecha
+    contador, contador_izquierda, contador_derecha = 0, 0, 0
     state = 1
     end = True
     while(end):
         if(state == 1):
-            cargarTexto()
-            x = input("/> ")
-            x = x.lower().split()
-            while(x[0] not in comandos_niveles[state - 1] or evaluar_comando(x, "alderaan")):
-                print("Comando no reconocido.")
-                x = input("/> ")
-                x = x.lower().split()
-            if(x[0] == "go"):
+            comando = juego(state)
+            if(comando[0] == "go"):
                 state = 2
                 print("")
                 cargarTextoIzquierda()
                 time.sleep(6)
-            elif(x[0] == "stay"):
+            elif(comando[0] == "stay"):
                 end = False
                 cargarTextoDerecha()
                 time.sleep(10)
                 imprimirFinal()
         elif(state == 2):
-            cargarTexto()
-            x = input("/> ")
-            x = x.lower().split()
-            while(x[0] not in comandos_niveles[state - 1] or evaluar_comando(x, "extraño") or evaluar_comando(x, "home")):
-                print("Comando no reconocido.")
-                x = input("/> ")
-                x = x.lower().split()
-            if(x[0] == "follow"):
+            comando = juego(state)
+            if(comando[0] == "follow"):
                 state = 3
                 print("")
                 cargarTextoIzquierda()
                 time.sleep(6)
-            elif(x[0] == "return"):
+            elif(comando[0] == "return"):
                 end = False
                 cargarTextoDerecha()
                 time.sleep(20)
                 imprimirFinal()
         elif(state == 3):
-            cargarTexto()
-            x = input("/> ")
-            x = x.lower().split()
-            while(x[0] not in comandos_niveles[0] or len(x) == 1 or x[1] != "extraño" or x[1] != "home"):
-                print("Comando no reconocido.")
-                x = input("/> ")
-                x = x.lower().split()
-        
+            comando = juego(state)
+            if(comando[0] == "take"):
+                cargarTextoIzquierda()
+                time.sleep(30)
+            elif(comando[0] == "straight"):
+                cargarTextoDerecha()
+                time.sleep(60)
+            state = 4
+        elif(state == 4):
+            comando = juego(state)
+            if(comando[0] == "go"):
+                cargarTextoDerecha()
+            elif(comando[0] == "send"):
+                cargarTextoIzquierda()
